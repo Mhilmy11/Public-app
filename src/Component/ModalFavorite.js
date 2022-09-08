32f2ad68bfdd25f632b512e023200345aa4a18a0
Hilmy Setiawanto <setiawantojr@gmail.com>
2022-09-08 11:30:35 +0700

    fix bug modal fav

--- src/Component/ModalFavorite.js
+++ src/Component/ModalFavorite.js
@@ -11,25 +11,27 @@ export default function ModalFavorite({
         <div>
             <div className=" bg-black opacity-50 h-screen w-full fixed"></div>
             <div className=" absolute flex justify-center items-center h-screen w-full">
-                <div className=" fixed">
-                    <Container>
-                        <div className=" w-full flex justify-end">
-                            <button onClick={() => setFavorite(false)} className=' font-bold text-white bg-red-600 rounded-full w-7 h-7 mb-2'>X</button>
-                        </div>
-                        <div className=" bg-cyan-800 w-full h-[700px] rounded-3xl">
-                            <h1 className=" p-4 text-5xl text-white">Your Favorite</h1>
-                            <div>
-                                {favoriteFromLS.map((api, i) => (
-                                    <div key={i} className=" mb-2">
-                                        <Card
-                                            api={api}
-                                        />
-                                    </div>
-                                ))}
+                <Container>
+                    <div className=" flex justify-center">
+                        <div className=" h-[750px] w-[900px]">
+                            <div className=" w-full flex justify-end">
+                                <button onClick={() => setFavorite(false)} className=' font-bold text-white bg-red-600 rounded-full w-7 h-7 mb-2'>X</button>
+                            </div>
+                            <div className=" bg-cyan-800 w-full rounded-3xl">
+                                <h1 className=" p-4 text-5xl text-white">Your Favorite</h1>
+                                <div className=" p-2">
+                                    {favoriteFromLS.map((api, i) => (
+                                        <div key={i} className=" mb-2">
+                                            <Card
+                                                api={api}
+                                            />
+                                        </div>
+                                    ))}
+                                </div>
                             </div>
                         </div>
-                    </Container>
-                </div>
+                    </div>
+                </Container>
             </div>
         </div>
     )
